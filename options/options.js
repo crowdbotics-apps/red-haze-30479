7bf9dc3d5fb0be6132e3bba1e9ba56341cb94826
Crowdbotics <team@crowdbotics.com>
2021-09-12 12:27:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "red_haze_30479Identifier",
+  url: "https://red_haze_30479Identifier.botics.co",
+  api: "https://red_haze_30479Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
